a69b0a5d7c6153b5d12ba798d0bdf0386ff06126
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-10-05 14:27:58 +0300

    Add Blog filter

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -4,11 +4,11 @@ import PageHead from "../../components/pageHead/PageHead";
 import MainLayout from "../../layouts/mainLayout";
 // Multi lang
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import axios from "../../lib/axios";
 import AppUrl from "../../lib/AppUrl";
 import PageTitleBanner from "../../components/UI/PageTitle";
-import BlogTopics from "../../sections/blog/BlogTopics";
+import BlogTopics, { filterArticles } from "../../sections/blog/BlogTopics";
 import BlogArticlesCards from "../../sections/blog/BlogArticlesCards";
 import { useTranslation } from "next-i18next";
 
@@ -32,20 +32,31 @@ export async function getStaticProps({ locale }) {
   };
 }
 
-export default function Home(props) {
+export default function Blog(props) {
   const { t } = useTranslation();
+  const [filters, setFilters] = useState(null);
 
   useEffect(() => {
     localStorage.setItem("menu", JSON.stringify(props.menu));
   }, []);
 
+  const filteredarticles = useMemo(
+    () => filterArticles(props.articles, filters),
+    [props.articles, filters]
+  );
+
   return (
     <div>
       <PageHead title="blog"></PageHead>
       <PageTitleBanner title={t("blogTitle")} subTitle={t("blogSubTitle")} />
-      <BlogTopics topics={props.topics} locale={props.locale} />
-      <BlogArticlesCards articles={props.articles} locale={props.locale} />
+      <BlogTopics
+        topics={props.topics}
+        locale={props.locale}
+        articles={props.articles}
+        setFilters={setFilters}
+      />
+      <BlogArticlesCards articles={filteredarticles} locale={props.locale} />
     </div>
   );
 }
-Home.Layout = MainLayout;
+Blog.Layout = MainLayout;
--- sections/blog/BlogTopics.js
+++ sections/blog/BlogTopics.js
@@ -1,10 +1,28 @@
+import { useTranslation } from "next-i18next";
 import styles from "../../styles/theme/main.module.scss";
 
-const BlogTopics = ({ topics, locale }) => {
+export function filterArticles(rows, filter) {
+  if (filter === null) {
+    return rows;
+  }
+  let articles = rows.filter((item) => item.topic["id"] === filter);
+  return articles;
+}
+
+const BlogTopics = ({ topics, locale, setFilters }) => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles["blog--topics--section"]}>
+      <div className={styles["body-2--light"]} onClick={() => setFilters(null)}>
+        {t("all")}
+      </div>
       {topics.map((item, index) => (
-        <div key={index} className={styles["body-2--light"]}>
+        <div
+          key={index}
+          className={styles["body-2--light"]}
+          onClick={() => setFilters(item.id)}
+        >
           {item.topic[locale]}
         </div>
       ))}
